c32824eb3d5ba895cd52602dd685221da6954105
Molda <inna.graur82@gmail.com>
2025-04-13 19:05:54 +0200

    hallo

--- task_manager_app/admin.py
+++ task_manager_app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Task, SubTask, Category
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'deadline', 'created_at')
+    list_filter = ('status', 'categories')
+    search_fields = ('title', 'description')
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'task', 'status', 'deadline', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('title', 'description')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- task_manager_app/models.py
+++ task_manager_app/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('in_progress', 'In progress'),
+        ('pending', 'Pending'),
+        ('blocked', 'Blocked'),
+        ('done', 'Done'),
+    ]
+
+    title = models.CharField(max_length=100, unique_for_date='deadline')
+    description = models.TextField()
+    categories = models.ManyToManyField(Category)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new')
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
+class SubTask(models.Model):
+    STATUS_CHOICES = Task.STATUS_CHOICES
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new')
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.task.title})"

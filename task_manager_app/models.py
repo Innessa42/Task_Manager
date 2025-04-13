3774d83fd987e379e3ce959f3a3cb4ff124fdf89
Molda <inna.graur82@gmail.com>
2025-04-13 19:32:04 +0200

    hallo

--- task_manager_app/models.py
+++ task_manager_app/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    class Meta:
+        db_table = "task_manager_category"
+        verbose_name = "Category"
+        unique_together = ("name",)
 
     def __str__(self):
         return self.name
@@ -23,6 +27,11 @@ class Task(models.Model):
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        db_table = "task_manager_task"
+        ordering = ('-created_at',)
+        verbose_name = "Task"
+        unique_together = ("title",)
 
     def __str__(self):
         return self.title
@@ -30,12 +39,19 @@ class Task(models.Model):
 class SubTask(models.Model):
     STATUS_CHOICES = Task.STATUS_CHOICES
 
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='new')
     deadline = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "task_manager_subtask"
+        ordering = ('-created_at',)
+        verbose_name = "SubTask"
+        unique_together = ("title",)
 
     def __str__(self):
         return f"{self.title} ({self.task.title})"
